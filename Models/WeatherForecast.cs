8a25c49f63ee5e714257232eed082afcb6b08033
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-05 11:53:52 -0600

    Add WeatherForecast model and controller with API endpoint

---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstApi.Models;
+
+public class WeatherForecast(DateOnly date, int temperatureC, string? summary)
+{
+    public DateOnly Date { get; set; } = date;
+    public int TemperatureC { get; set; } = temperatureC;
+    public string? Summary { get; set; } = summary;
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
