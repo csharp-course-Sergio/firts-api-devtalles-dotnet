53608eec5d1a5bb609a530aaba4e07b22cbfa6ca
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-05 12:05:27 -0600

    Add Task model and TaskDataStore service with sample data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -29,7 +27,6 @@ app.Use(async (context, next) =>
         await next();
     }
 });
-
 
 app.MapControllers();
 
